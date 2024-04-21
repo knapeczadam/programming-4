cae9948f7b2e0da9bc1c4c9961def8e16316b1c3
Ádám Knapecz <knapecz.adam@gmail.com>
2024-04-21 16:59:31 +0200

    Enable V-sync Add fixed update Command  can clone itself Fix input handling in SDL impl Change intermediate and output dirs Add move component to players, bind reset move commands to inputs

---
+++ Minigin/component/implementation/move_component.cpp
@@ -0,0 +1,23 @@
+﻿#include "move_component.h"
+
+// Project includes
+#include "game_object.h"
+
+namespace dae
+{
+    void move_component::fixed_update()
+    {
+        if (is_moving_)
+        {
+            auto const go = get_owner();
+            auto const new_pos = go->get_local_position() + direction_ * speed_;
+            go->set_local_position(new_pos);
+        }
+    }
+
+    void move_component::set_direction(glm::vec3 const &direction)
+    {
+        is_moving_ = true;
+        direction_ = direction;
+    }
+}
