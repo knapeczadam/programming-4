21c676e88630ff6fdca0981a32fb848d168ec963
Ádám Knapecz <knapecz.adam@gmail.com>
2024-05-14 15:36:09 +0200

    Fix: add missing factory class

---
+++ Qbert/core/factory.cpp
@@ -0,0 +1,67 @@
+﻿#include "factory.h"
+
+// Project includes
+#include "component/player/face_component.h"
+#include "component/player/fall_component.h"
+#include "component/player/health_component.h"
+#include "component/player/jump_component.h"
+#include "component/player/player_state_component.h"
+#include "component/player/position_idx_component.h"
+#include "component/player/score_counter_component.h"
+#include "input/game_object_commands.h"
+#include "minigin/component/rendering/sprite_component.h"
+#include "minigin/core/game_object.h"
+#include "minigin/core/scene.h"
+#include "minigin/input/input_manager.h"
+
+namespace qbert
+{
+    auto factory::character::create_player(player_config_info const &config) -> player_info
+    {
+        player_info info;
+        info.go_ptr = config.scene_ptr->add_game_object(config.name);
+        info.go_ptr->set_parent(config.parent_go_ptr);
+        info.go_ptr->set_local_position(config.local_position);
+        info.go_ptr->add_component<mngn::sprite_component>(config.sprite_id, config.texture_id);
+        info.go_ptr->add_component<jump_component>();
+        info.go_ptr->add_component<face_component>();
+        info.go_ptr->add_component<player_state_component>();
+        info.go_ptr->add_component<fall_component>();
+        info.position_idx_comp_ptr  = info.go_ptr->add_component<position_idx_component>(config.row_idx, config.col_idx);
+        info.health_comp_ptr        = info.go_ptr->add_component<health_component>();
+        info.score_counter_comp_ptr = info.go_ptr->add_component<score_counter_component>();
+
+        auto left_command = std::make_unique<jump_command>(info.go_ptr, -1, -1);
+        auto right_command = std::make_unique<jump_command>(info.go_ptr, 1, 1);
+        auto up_command = std::make_unique<jump_command>(info.go_ptr, -1, 0);
+        auto down_command = std::make_unique<jump_command>(info.go_ptr, 1, 0);
+
+        mngn::input_manager::instance().bind_command(config.left_command.input_type, config.left_command.input_state, config.left_command.input, std::move(left_command));
+        mngn::input_manager::instance().bind_command(config.right_command.input_type, config.right_command.input_state, config.right_command.input, std::move(right_command));
+        mngn::input_manager::instance().bind_command(config.up_command.input_type, config.up_command.input_state, config.up_command.input, std::move(up_command));
+        mngn::input_manager::instance().bind_command(config.down_command.input_type, config.down_command.input_state, config.down_command.input, std::move(down_command));
+
+        if (config.left_command_alt)
+        {
+            auto left_command_alt = std::make_unique<jump_command>(info.go_ptr, -1, -1);
+            mngn::input_manager::instance().bind_command(config.left_command_alt.value().input_type, config.left_command_alt.value().input_state, config.left_command_alt.value().input, std::move(left_command_alt));
+        }
+        if (config.right_command_alt)
+        {
+            auto right_command_alt = std::make_unique<jump_command>(info.go_ptr, 1, 1);
+            mngn::input_manager::instance().bind_command(config.right_command_alt.value().input_type, config.right_command_alt.value().input_state, config.right_command_alt.value().input, std::move(right_command_alt));
+        }
+        if (config.up_command_alt)
+        {
+            auto up_command_alt = std::make_unique<jump_command>(info.go_ptr, -1, 0);
+            mngn::input_manager::instance().bind_command(config.up_command_alt.value().input_type, config.up_command_alt.value().input_state, config.up_command_alt.value().input, std::move(up_command_alt));
+        }
+        if (config.down_command_alt)
+        {
+            auto down_command_alt = std::make_unique<jump_command>(info.go_ptr, 1, 0);
+            mngn::input_manager::instance().bind_command(config.down_command_alt.value().input_type, config.down_command_alt.value().input_state, config.down_command_alt.value().input, std::move(down_command_alt));
+        }
+
+        return info;
+    }
+}
---
+++ Qbert/core/factory.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+
+// Standard includes
+#include <string>
+
+// GLM includes
+#include <optional>
+#include <glm/glm.hpp>
+
+// Forward declarations
+namespace mngn
+{
+    class game_object;
+    class scene;
+
+    enum input : int;
+    enum class input_type;
+    enum class input_state;
+}
+
+namespace qbert
+{
+    // Forward declarations
+    enum qbert_resource : int;
+    enum qbert_sprite   : int;
+    
+    
+    class position_idx_component;
+    class health_component;
+    class score_counter_component;
+    
+    struct factory
+    {
+        struct character
+        {
+            
+            struct player_config_info
+            {
+            private:
+                struct command
+                {
+                    mngn::input_type  input_type;
+                    mngn::input_state input_state;
+                    mngn::input       input;
+                };
+                
+            public:
+                
+                mngn::scene            *scene_ptr;
+                mngn::game_object      *parent_go_ptr;
+                std::string            name;
+                glm::vec2              local_position;
+                qbert_sprite           sprite_id;
+                qbert_resource         texture_id;
+                int                    row_idx;
+                int                    col_idx;
+                command                left_command;
+                command                right_command;
+                command                up_command;
+                command                down_command;
+                std::optional<command> left_command_alt;
+                std::optional<command> right_command_alt;
+                std::optional<command> up_command_alt;
+                std::optional<command> down_command_alt;
+            };
+
+            struct player_info
+            {
+                mngn::game_object       *go_ptr;
+                position_idx_component  *position_idx_comp_ptr;
+                health_component        *health_comp_ptr;
+                score_counter_component *score_counter_comp_ptr;
+            };
+            
+            static auto create_player(player_config_info const& config) -> player_info;
+        };
+        
+        struct level
+        {
+            
+        };
+
+        struct my_struct
+        {
+            
+        };
+    };
+}
