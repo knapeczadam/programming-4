765534871f2307c2577455b6c0a80c9a48e99e58
Ádám Knapecz <knapecz.adam@gmail.com>
2024-06-09 04:01:18 +0200

    Minor fix

--- Minigin/minigin/sound/sound_effect.cpp
+++ Minigin/minigin/sound/sound_effect.cpp
@@ -30,7 +30,6 @@ namespace mngn
 
     auto sound_effect::play(int const loops) const -> bool
     {
-        std::cout << '#' << std::this_thread::get_id() << " thread: calling sound_effect::play" << '\n';
         // Don't save the channel as a data member,
         // because when it stops playing the channel becomes free
         // and available for usage by other effects
